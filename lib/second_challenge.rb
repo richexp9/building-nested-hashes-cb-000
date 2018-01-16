fa67593b39561d350c78c8e06b5bec08af09208d
Richard Loera <richexp9@gmx.com>
2018-01-16 08:23:40 +0000

    Done.

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -1,11 +1,15 @@
 def second_challenge
   epic_tragedy = {
     :montague => {
+      patriarch: {},
+      matriarch: {},
+      hero: {},
+      hero_friends: []
       #your key/value pairs here!
-      }, 
+      },
     :capulet => {
       #your key/value pairs here!
       }}
 
-  
+
 end
