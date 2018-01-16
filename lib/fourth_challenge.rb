f085abd30aa70958a2cb019567c307aff16f1f9a
Richard Loera <richexp9@gmx.com>
2018-01-16 10:08:05 +0000

    automatically backed up by learn

--- lib/fourth_challenge.rb
+++ lib/fourth_challenge.rb
@@ -12,7 +12,6 @@ def fourth_challenge
         {name: "Mercutio",
           age: "18",
           attitude: "hot-headed"
-
         }
       ]
    },
@@ -21,7 +20,10 @@ def fourth_challenge
       :matriarch => {name: "Lady Capulet", age: "51"},
       :heroine => {name: "Juliet", age: "15", status: "alive"},
       :heroine_friends => [
-        #your hashes here!
+      {
+        name: "Steven",
+        
+      }
       ]
    }
 }
